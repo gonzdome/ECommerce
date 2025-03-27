f1723c863f5520ce5a622060a5d482bee00df3da
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-26 23:43:45 -0300

    feat: add DTOs for Product and Purchase

---
+++ ECommerce/ECommerce.ProductAPI/DTO/ProductDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommerce.ProductAPI.DTO;
+
+public class ProductDTO
+{
+    public Guid Id { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public bool Active { get; set; }
+    public bool Excluded { get; set; }
+}
