5ad0f1d6eb3486dfaa71c807cc2f39db89244f84
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-26 23:37:04 -0300

    feat: add ProductService and Interface with started crud methods, still in progress

---
+++ ECommerce/ECommerce.ProductAPI/Services/Interfaces/IProductService.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.ProductAPI.Services.Interfaces;
+
+public interface IProductService
+{
+    public IEnumerable<ProductDTO> GetProducts();
+    public ProductDTO DetailProduct(string id);
+    public ProductDTO CreateProduct(ProductDTO product);
+    public ProductDTO UpdateProduct(string id, ProductDTO product);
+    public ProductDTO DeleteProduct(string id);
+}
---
+++ ECommerce/ECommerce.ProductAPI/Services/ProductService.cs
@@ -0,0 +1,60 @@
+﻿
+
+namespace ECommerce.ProductAPI.Services;
+
+public class ProductService : IProductService
+{
+    private readonly IUnitOfWork _unitOfWork;
+    ProductService(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public ProductDTO DetailProduct(string id)
+    {
+        var product = GetAndReturnProduct(id);
+
+        return product.MapToProductDTO();
+    }
+
+    public IEnumerable<ProductDTO> GetProducts()
+    {
+        IEnumerable<Product> products = _unitOfWork.ProductRepository.GetAll();
+
+        var productsMapped = products.Select(p => p.MapToProductDTO());
+
+        return productsMapped;
+    }
+
+
+    public ProductDTO CreateProduct(ProductDTO productToCreate)
+    {
+        Product product = productToCreate.MapToProduct();
+
+        var productCreated = _unitOfWork.ProductRepository.Create(productToCreate);
+
+        _unitOfWork.Commit();
+
+        return productCreated.MapToProductDTO();
+    }
+
+    public ProductDTO DeleteProduct(string id)
+    {
+
+        var product = _unitOfWork.ProductRepository.DeleteProduct();
+    }
+
+    public ProductDTO UpdateProduct(string id, ProductDTO product)
+    {
+        var product = _unitOfWork.ProductRepository.UpdateProduct();
+    }
+
+    private Product? GetAndReturnProduct(string id)
+    {
+        var product = _unitOfWork.ProductRepository.Get(p => p.ProdutoId == id);
+        if (product == null)
+            throw new Exception($"Product with id {id} not found!");
+
+        return product;
+    }
+}
