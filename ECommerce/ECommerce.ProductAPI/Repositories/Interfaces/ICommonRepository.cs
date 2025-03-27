a99c9462fa65e6ddaca9ac1366f05540d333978e
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-26 23:38:32 -0300

    feat: add CommonRepository and Interface with started crud methods

---
+++ ECommerce/ECommerce.ProductAPI/Repositories/Interfaces/ICommonRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace ECommerce.ProductAPI.Repositories.Interfaces;
+
+public interface ICommonRepository<T>
+{
+    IEnumerable<T> GetAll();
+    T? Details(Expression<Func<T, bool>> predicate);
+    T Create(T entity);
+    T Update(T entity);
+    T Delete(T entity);
+}
