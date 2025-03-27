2c4fb1846f2108ce13991ccee1f27807bc548d97
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-26 23:44:06 -0300

    feat: add GlobalUsing

---
+++ ECommerce/ECommerce.ProductAPI/GlobalUsing.cs
@@ -0,0 +1,12 @@
+﻿global using Microsoft.AspNetCore.Mvc;
+
+global using ECommerce.ProductAPI.Context;
+global using Microsoft.EntityFrameworkCore;
+
+global using ECommerce.ProductAPI.Models.Entities;
+
+global using ECommerce.ProductAPI.DTO;
+global using ECommerce.ProductAPI.DTO.DTOMapping;
+
+global using ECommerce.ProductAPI.Services.Interfaces;
+global using ECommerce.ProductAPI.Repositories.Interfaces;
