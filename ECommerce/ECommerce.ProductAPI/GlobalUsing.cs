4fdaa3d1662113ad289267c69416a0d390aa1ddb
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-27 21:52:48 -0300

    feat: add new namespaces at GlobalUsing

--- ECommerce/ECommerce.ProductAPI/GlobalUsing.cs
+++ ECommerce/ECommerce.ProductAPI/GlobalUsing.cs
@@ -8,5 +8,9 @@ global using ECommerce.ProductAPI.Models.Entities;
 global using ECommerce.ProductAPI.DTO;
 global using ECommerce.ProductAPI.DTO.DTOMapping;
 
+global using ECommerce.ProductAPI.Services;
+global using ECommerce.ProductAPI.Repositories;
+
 global using ECommerce.ProductAPI.Services.Interfaces;
 global using ECommerce.ProductAPI.Repositories.Interfaces;
+
