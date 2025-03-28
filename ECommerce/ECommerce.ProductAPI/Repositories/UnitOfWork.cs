4b3c75f2afd5f6ef0a97862b88654626bb12e85d
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-27 21:51:58 -0300

    feat: add on UnitOfWork the configuration for the instances of the repositories

--- ECommerce/ECommerce.ProductAPI/Repositories/UnitOfWork.cs
+++ ECommerce/ECommerce.ProductAPI/Repositories/UnitOfWork.cs
@@ -1,15 +1,25 @@
 ﻿namespace ECommerce.ProductAPI.Repositories;
 
-public class UnitOfWork
+public class UnitOfWork : IUnitOfWork
 {
     public AppDbContext _context;
     private IProductRepository? _productRepository;
     private IPurchaseRepository? _purchaseRepository;
 
-    public UnitOfWork(IProductRepository? productRepository, IPurchaseRepository purchaseRepository)
+    public UnitOfWork(IProductRepository? productRepository, IPurchaseRepository? purchaseRepository)
     {
         _productRepository = productRepository;
         _purchaseRepository = purchaseRepository;
+    }
+
+    public IProductRepository ProductRepository
+    {
+        get { return _productRepository = _productRepository ?? new ProductRepository(_context); }
+    }
+
+    public IPurchaseRepository PurchaseRepository
+    {
+        get { return _purchaseRepository = _purchaseRepository ?? new PurchaseRepository(_context); }
     }
 
     public void Commit()
