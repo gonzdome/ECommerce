3f68941ffe28b6a0e714c30e67e30cc3d9ae7252
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-27 21:46:26 -0300

    refactor: changes on PurchaseService and Interface, variables and Update/Delete methods changed names

--- ECommerce/ECommerce.ProductAPI/Services/Interfaces/IPurchaseService.cs
+++ ECommerce/ECommerce.ProductAPI/Services/Interfaces/IPurchaseService.cs
@@ -5,6 +5,6 @@ public interface IPurchaseService
     public IEnumerable<PurchaseDTO> GetPurchases();
     public PurchaseDTO DetailPurchase(string id);
     public PurchaseDTO CreatePurchase(PurchaseDTO purchase);
-    public PurchaseDTO UpdatePurchase(string id, PurchaseDTO purchase);
-    public PurchaseDTO DeletePurchase(string id);
+    public PurchaseDTO UpdatePurchaseById(string id, PurchaseDTO purchase);
+    public PurchaseDTO DeletePurchaseById(string id);
 }
--- ECommerce/ECommerce.ProductAPI/Services/PurchaseService.cs
+++ ECommerce/ECommerce.ProductAPI/Services/PurchaseService.cs
@@ -1,5 +1,69 @@
-﻿namespace ECommerce.ProductAPI.Services;
+﻿
+namespace ECommerce.ProductAPI.Services;
 
-public class PurchaseService
+public class PurchaseService : IPurchaseService
 {
+    private readonly IUnitOfWork _unitOfWork;
+    PurchaseService(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public PurchaseDTO DetailPurchase(string id)
+    {
+        var purchase = GetAndReturnPurchase(id);
+
+        return purchase.MapToPurchaseDTO();
+    }
+
+    public IEnumerable<PurchaseDTO> GetPurchases()
+    {
+        IEnumerable<Purchase> purchases = _unitOfWork.PurchaseRepository.GetAll();
+
+        var purchasesMapped = purchases.Select(p => p.MapToPurchaseDTO());
+
+        return purchasesMapped;
+    }
+
+    public PurchaseDTO CreatePurchase(PurchaseDTO purchaseToCreate)
+    {
+        Purchase mappedToPurchase = purchaseToCreate.MapToPurchase();
+
+        var purchaseCreated = _unitOfWork.PurchaseRepository.Create(mappedToPurchase);
+
+        _unitOfWork.Commit();
+
+        return purchaseCreated.MapToPurchaseDTO();
+    }
+
+    public PurchaseDTO DeletePurchaseById(string id)
+    {
+        var foundPurchase = GetAndReturnPurchase(id);
+
+        _unitOfWork.PurchaseRepository.Delete(foundPurchase);
+
+        _unitOfWork.Commit();
+
+        return foundPurchase.MapToPurchaseDTO();
+    }
+
+    public PurchaseDTO UpdatePurchaseById(string id, PurchaseDTO purchase)
+    {
+        var foundPurchase = GetAndReturnPurchase(id);
+
+        _unitOfWork.PurchaseRepository.Update(foundPurchase);
+
+        _unitOfWork.Commit();
+
+        return foundPurchase.MapToPurchaseDTO();
+    }
+
+    private Purchase? GetAndReturnPurchase(string id)
+    {
+        var purchase = _unitOfWork.PurchaseRepository.Details(p => p.Id == Guid.Parse(id));
+        if (purchase == null)
+            throw new Exception($"Purchase with id {id} not found!");
+
+        return purchase;
+    }
 }
