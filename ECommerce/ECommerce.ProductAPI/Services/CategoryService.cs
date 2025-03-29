c60011a5f228d76ec56e83094e7800d88c3dad12
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-29 17:40:12 -0300

    refactor: turn methods from Category flow to async

--- ECommerce/ECommerce.ProductAPI/Services/CategoryService.cs
+++ ECommerce/ECommerce.ProductAPI/Services/CategoryService.cs
@@ -11,23 +11,23 @@ public class CategoryService : ICategoryService
         _unitOfWork = unitOfWork;
     }
 
-    public CategoryDTO DetailCategory(string id)
+    public async Task<CategoryDTO> DetailCategoryById(string id)
     {
-        var category = GetAndReturnCategory(id);
+        var category = await GetAndReturnCategory(id);
 
         return category.MapToCategoryDTO();
     }
 
-    public IEnumerable<CategoryDTO> GetCategories()
+    public async Task<IEnumerable<CategoryDTO>> GetCategories()
     {
-        IEnumerable<Category> categories = _unitOfWork.CategoryRepository.GetAll().Where(c => !c.Excluded && c.Active);
+        IEnumerable<Category> categories = (await _unitOfWork.CategoryRepository.GetAll()).Where(p => !p.Excluded && p.Active);
 
         var categoriesMapped = categories.Select(p => p.MapToCategoryDTO());
 
         return categoriesMapped;
     }
 
-    public CategoryDTO CreateCategory(CategoryDTO categoryToCreate)
+    public async Task<CategoryDTO> CreateCategory(CategoryDTO categoryToCreate)
     {
         categoryToCreate.Id = Guid.NewGuid();
         categoryToCreate.CreatedAt = DateTime.Now;
@@ -37,43 +37,43 @@ public class CategoryService : ICategoryService
 
         Category mappedToCategory = categoryToCreate.MapToCategory();
 
-        var categoryCreated = _unitOfWork.CategoryRepository.Create(mappedToCategory);
+        var categoryCreated = await _unitOfWork.CategoryRepository.Create(mappedToCategory);
 
-        _unitOfWork.Commit();
+        await _unitOfWork.Commit();
 
         return categoryCreated.MapToCategoryDTO();
     }
 
-    public CategoryDTO UpdateCategoryById(string id, CategoryDTO categoryToUpdate)
+    public async Task<CategoryDTO> UpdateCategoryById(string id, CategoryDTO categoryToUpdate)
     {
-        GetAndReturnCategory(id);
+        await GetAndReturnCategory(id);
 
         categoryToUpdate.Id = Guid.Parse(id);
         categoryToUpdate.UpdatedAt = DateTime.Now;
         var mappedCategory = categoryToUpdate.MapToCategory();
 
-        _unitOfWork.CategoryRepository.Update(mappedCategory);
+        await _unitOfWork.CategoryRepository.Update(mappedCategory);
 
-        _unitOfWork.Commit();
+        await _unitOfWork.Commit();
 
         return mappedCategory.MapToCategoryDTO();
     }
 
 
-    public CategoryDTO DeleteCategoryById(string id)
+    public async Task<CategoryDTO> DeleteCategoryById(string id)
     {
-        var foundCategory = GetAndReturnCategory(id);
+        var foundCategory = await GetAndReturnCategory(id);
 
-        _unitOfWork.CategoryRepository.Delete(foundCategory);
+        await _unitOfWork.CategoryRepository.Delete(foundCategory);
 
-        _unitOfWork.Commit();
+        await _unitOfWork.Commit();
 
         return foundCategory.MapToCategoryDTO();
     }
 
-    private Category? GetAndReturnCategory(string id)
+    private async Task<Category> GetAndReturnCategory(string id)
     {
-        var category = _unitOfWork.CategoryRepository.Details(c => c.Id == Guid.Parse(id) && !c.Excluded && c.Active);
+        var category = await _unitOfWork.CategoryRepository.Details(c => c.Id == Guid.Parse(id) && !c.Excluded && c.Active);
         if (category == null)
             throw new Exception($"Category with id {id} not found!");
 
--- ECommerce/ECommerce.ProductAPI/Services/Interfaces/ICategoryService.cs
+++ ECommerce/ECommerce.ProductAPI/Services/Interfaces/ICategoryService.cs
@@ -2,9 +2,9 @@
 
 public interface ICategoryService
 {
-    public IEnumerable<CategoryDTO> GetCategories();
-    public CategoryDTO DetailCategory(string id);
-    public CategoryDTO CreateCategory(CategoryDTO purchase);
-    public CategoryDTO UpdateCategoryById(string id, CategoryDTO purchase);
-    public CategoryDTO DeleteCategoryById(string id);
+    public Task<IEnumerable<CategoryDTO>> GetCategories();
+    public Task<CategoryDTO> DetailCategoryById(string id);
+    public Task<CategoryDTO> CreateCategory(CategoryDTO purchase);
+    public Task<CategoryDTO> UpdateCategoryById(string id, CategoryDTO purchase);
+    public Task<CategoryDTO> DeleteCategoryById(string id);
 }
