842b23a7a21264c3255200f7ade1109ad45cbfe1
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-29 17:42:19 -0300

    refactor: add DataAnnotations to GlobalUsing

--- ECommerce/ECommerce.ProductAPI/GlobalUsing.cs
+++ ECommerce/ECommerce.ProductAPI/GlobalUsing.cs
@@ -1,4 +1,5 @@
 ﻿global using Microsoft.AspNetCore.Mvc;
+global using System.ComponentModel.DataAnnotations;
 
 global using ECommerce.ProductAPI.Context;
 global using Microsoft.EntityFrameworkCore;
