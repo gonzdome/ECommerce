e14cc5e6b0101720e8892feb903bb8849cdab874
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-29 17:52:32 -0300

    feat: add Product ViewModels to AggregatorWebAPI

---
+++ ECommerce/ECommerce.AggregatorWebAPI/Models/ViewModels/Product/CreateProductViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace ECommerce.AggregatorWebAPI.Models.ViewModels.Product;
+
+public class CreateProductViewModel
+{
+    [MinLength(3)]
+    [MaxLength(100)]
+    public string Name { get; set; }
+
+    [MinLength(3)]
+    [MaxLength(200)]
+    public string Description { get; set; }
+
+    public int Stock { get; set; }
+
+    [RegularExpression(@"\d{1,20}(\.\d{1,2})?", ErrorMessage = "Invalid format for price!")]
+    public double Price { get; set; }
+
+    public Guid CategoryId { get; set; }
+}
---
+++ ECommerce/ECommerce.AggregatorWebAPI/Models/ViewModels/Product/DetailProductViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace ECommerce.AggregatorWebAPI.Models.ViewModels.Product;
+
+public class DetailProductViewModel
+{
+    public Guid Id { get; set; }
+
+    [MinLength(3)]
+    [MaxLength(100)]
+    public string Name { get; set; }
+
+    [MinLength(3)]
+    [MaxLength(200)]
+    public string Description { get; set; }
+
+    public int Stock { get; set; }
+
+    [RegularExpression(@"\d{1,20}(\.\d{1,2})?", ErrorMessage = "Invalid format for price!")]
+    public double Price { get; set; }
+
+    public Guid CategoryId { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+}
