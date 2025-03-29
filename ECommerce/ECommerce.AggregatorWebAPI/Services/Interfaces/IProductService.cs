cd0bc00e99d0c42984f8ea7733f7feac03133bce
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-29 17:52:08 -0300

    feat: add ProductService/Interface to AggregatorWebAPI

---
+++ ECommerce/ECommerce.AggregatorWebAPI/Services/Interfaces/IProductService.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.AggregatorWebAPI.Services.Interfaces;
+
+public interface IProductService
+{
+    public Task<IEnumerable<GetProductsViewModel>> GetProducts();
+    public Task<DetailProductViewModel> DetailProduct(string id);
+    public Task<CreateProductViewModel> CreateProduct(CreateProductViewModel product);
+    public Task<UpdateProductViewModel> UpdateProductById(string id, UpdateProductViewModel product);
+    public Task<DetailProductViewModel> DeleteProductById(string id);
+}
---
+++ ECommerce/ECommerce.AggregatorWebAPI/Services/ProductService.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+
+namespace ECommerce.AggregatorWebAPI.Services;
+
+public class ProductService : IProductService
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+    private const string apiEndpoint = "/product/";
+    private readonly JsonSerializerOptions _options;
+
+    public ProductService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+        _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+    }
+
+    public async Task<CreateProductViewModel> CreateProduct(CreateProductViewModel productToCreate)
+    {
+
+
+        return new CreateProductViewModel() { };
+    }
+
+    public async Task<IEnumerable<GetProductsViewModel>> GetProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<DetailProductViewModel> DetailProduct(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<UpdateProductViewModel> UpdateProductById(string id, UpdateProductViewModel product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<DetailProductViewModel> DeleteProductById(string id)
+    {
+        throw new NotImplementedException();
+    }
+}
