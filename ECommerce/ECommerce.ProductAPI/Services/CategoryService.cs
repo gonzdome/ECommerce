5783061b71fef6e3512d79ec823055a33018a692
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-28 22:43:56 -0300

    feat: add CategoryService and Interface

---
+++ ECommerce/ECommerce.ProductAPI/Services/CategoryService.cs
@@ -0,0 +1,69 @@
+﻿namespace ECommerce.ProductAPI.Services;
+
+public class CategoryService : ICategoryService
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CategoryService(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public CategoryDTO DetailCategory(string id)
+    {
+        var category = GetAndReturnCategory(id);
+
+        return category.MapToCategoryDTO();
+    }
+
+    public IEnumerable<CategoryDTO> GetCategories()
+    {
+        IEnumerable<Category> categories = _unitOfWork.CategoryRepository.GetAll();
+
+        var categoriesMapped = categories.Select(p => p.MapToCategoryDTO());
+
+        return categoriesMapped;
+    }
+
+    public CategoryDTO CreateCategory(CategoryDTO categoryToCreate)
+    {
+        Category mappedToCategory = categoryToCreate.MapToCategory();
+
+        var categoryCreated = _unitOfWork.CategoryRepository.Create(mappedToCategory);
+
+        _unitOfWork.Commit();
+
+        return categoryCreated.MapToCategoryDTO();
+    }
+
+    public CategoryDTO DeleteCategoryById(string id)
+    {
+        var foundCategory = GetAndReturnCategory(id);
+
+        _unitOfWork.CategoryRepository.Delete(foundCategory);
+
+        _unitOfWork.Commit();
+
+        return foundCategory.MapToCategoryDTO();
+    }
+
+    public CategoryDTO UpdateCategoryById(string id, CategoryDTO category)
+    {
+        var foundCategory = GetAndReturnCategory(id);
+
+        _unitOfWork.CategoryRepository.Update(foundCategory);
+
+        _unitOfWork.Commit();
+
+        return foundCategory.MapToCategoryDTO();
+    }
+
+    private Category? GetAndReturnCategory(string id)
+    {
+        var category = _unitOfWork.CategoryRepository.Details(p => p.Id == Guid.Parse(id));
+        if (category == null)
+            throw new Exception($"Category with id {id} not found!");
+
+        return category;
+    }
+}
---
+++ ECommerce/ECommerce.ProductAPI/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.ProductAPI.Services.Interfaces;
+
+public interface ICategoryService
+{
+    public IEnumerable<CategoryDTO> GetCategories();
+    public CategoryDTO DetailCategory(string id);
+    public CategoryDTO CreateCategory(CategoryDTO purchase);
+    public CategoryDTO UpdateCategoryById(string id, CategoryDTO purchase);
+    public CategoryDTO DeleteCategoryById(string id);
+}
