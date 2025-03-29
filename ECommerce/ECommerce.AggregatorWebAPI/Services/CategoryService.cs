0866c92fa2f29482b7f7450bf75ef292acba53b3
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-29 17:50:13 -0300

    feat: add CategoryService/Interface to AggregatorWebAPI

---
+++ ECommerce/ECommerce.AggregatorWebAPI/Services/CategoryService.cs
@@ -0,0 +1,42 @@
+﻿using ECommerce.AggregatorWebAPI.Models.ViewModels;
+using System.Text.Json;
+
+namespace ECommerce.AggregatorWebAPI.Services;
+
+public class CategoryService : ICategoryService
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+    private const string apiEndpoint = "/category/";
+    private readonly JsonSerializerOptions _options;
+
+    public CategoryService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+        _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+    }
+
+    public async Task<IEnumerable<GetCategoriesViewModel>> GetCategories()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<DetailCategoryViewModel> DetailCategoryById(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<CreateCategoryViewModel> CreateCategory(CreateCategoryViewModel product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<UpdateCategoryViewModel> UpdateCategoryById(string id, UpdateCategoryViewModel product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<DetailCategoryViewModel> DeleteCategoryById(string id)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ ECommerce/ECommerce.AggregatorWebAPI/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.AggregatorWebAPI.Services.Interfaces;
+
+public interface ICategoryService
+{
+    public Task<IEnumerable<GetCategoriesViewModel>> GetCategories();
+    public Task<DetailCategoryViewModel> DetailCategoryById(string id);
+    public Task<CreateCategoryViewModel> CreateCategory(CreateCategoryViewModel product);
+    public Task<UpdateCategoryViewModel> UpdateCategoryById(string id, UpdateCategoryViewModel product);
+    public Task<DetailCategoryViewModel> DeleteCategoryById(string id);
+}
