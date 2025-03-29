3c37356302042f9ad4859ac5591c94e270523fae
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-29 17:49:05 -0300

    feat: add AggregatorWebAPI

---
+++ ECommerce/ECommerce.AggregatorWebAPI/GlobalUsing.cs
@@ -0,0 +1,11 @@
+﻿global using System.Text;
+global using Microsoft.AspNetCore.Mvc;
+global using System.ComponentModel.DataAnnotations;
+
+global using ECommerce.AggregatorWebAPI.Models.ViewModels.Product;
+global using ECommerce.AggregatorWebAPI.Models.ViewModels.Category;
+
+global using ECommerce.AggregatorWebAPI.Services;
+global using ECommerce.AggregatorWebAPI.Services.Interfaces;
+
+global using ECommerce.APIHandler;
