8e3f3c0a947d22f47cfa2e59fef6686df739a1a8
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-28 22:45:21 -0300

    feat: add CategoryDTO and Mapping

---
+++ ECommerce/ECommerce.ProductAPI/DTO/CategoryDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.ProductAPI.DTO;
+
+public class CategoryDTO
+{
+    public Guid Id { get; set; }
+
+    [MinLength(3)]
+    [MaxLength(100)]
+    public string Name { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public bool Active { get; set; }
+    public bool Excluded { get; set; }
+}
---
+++ ECommerce/ECommerce.ProductAPI/DTO/Mapping/CategoryDTOMappingExtension.cs
@@ -0,0 +1,36 @@
+﻿namespace ECommerce.ProductAPI.DTO.DTOMapping;
+
+public static class CategoryDTOMappingExtension
+{
+    public static CategoryDTO MapToCategoryDTO(this Category category)
+    {
+        if (category == null)
+            return null;
+
+        return new CategoryDTO()
+        {
+            Id = category.Id,
+            Name = category.Name,
+            CreatedAt = category.CreatedAt,
+            UpdatedAt = category.UpdatedAt,
+            Active = category.Active,
+            Excluded = category.Excluded,
+        };
+    }
+
+    public static Category MapToCategory(this CategoryDTO categoryDTO)
+    {
+        if (categoryDTO == null)
+            return null;
+
+        return new Category()
+        {
+            Id = categoryDTO.Id,
+            Name = categoryDTO.Name,
+            CreatedAt = categoryDTO.CreatedAt,
+            UpdatedAt = categoryDTO.UpdatedAt,
+            Active = categoryDTO.Active,
+            Excluded = categoryDTO.Excluded,
+        };
+    }
+}
