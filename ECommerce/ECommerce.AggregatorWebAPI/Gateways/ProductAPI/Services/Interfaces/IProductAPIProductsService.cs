ee1202ddf8a296a5e874a0c213dbe24ae6999a53
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-30 16:54:22 -0300

    feat: add to AggregatorWebAPI the Service/Interface to deal with Products Gateway

---
+++ ECommerce/ECommerce.AggregatorWebAPI/Gateways/ProductAPI/Services/Interfaces/IProductAPIProductsService.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.AggregatorWebAPI.Gateways.ProductAPI.Services.Interfaces;
+
+public interface IProductAPIProductsService
+{
+    public Task<GetProductsViewModelResponse> ProductAPIGetProducts();
+    public Task<DetailProductViewModelResponse> ProductAPIDetailProductById(string id);
+    public Task<CreateProductViewModelResponse> ProductAPICreateProduct(CreateProductViewModel product);
+    public Task<UpdateProductViewModelResponse> ProductAPIUpdateProductById(UpdateProductViewModel product);
+    public Task<DetailProductViewModelResponse> ProductAPIDeleteProductById(string id);
+}
---
+++ ECommerce/ECommerce.AggregatorWebAPI/Gateways/ProductAPI/Services/ProductAPIProductsService.cs
@@ -0,0 +1,38 @@
+﻿namespace ECommerce.AggregatorWebAPI.Gateways.ProductAPI.Services;
+
+public class ProductAPIProductsService : ProductAPIProductsGateway, IProductAPIProductsService
+{
+    public ProductAPIProductsService(APIClient apiClient) : base(apiClient)
+    {
+    }
+
+    public async Task<DetailProductViewModelResponse> ProductAPIDetailProductById(string id)
+    {
+        var productDetails = await DetailProductById(id);
+        return productDetails;
+    }
+
+    public async Task<GetProductsViewModelResponse> ProductAPIGetProducts()
+    {
+        var productsListed = await GetProducts();
+        return productsListed;
+    }
+
+    public async Task<CreateProductViewModelResponse> ProductAPICreateProduct(CreateProductViewModel product)
+    {
+        var createdProduct = await CreateProduct(product);
+        return createdProduct;
+    }
+
+    public async Task<UpdateProductViewModelResponse> ProductAPIUpdateProductById(UpdateProductViewModel product)
+    {
+        var updatedProduct = await UpdateProductById(product);
+        return updatedProduct;
+    }
+
+    public async Task<DetailProductViewModelResponse> ProductAPIDeleteProductById(string id)
+    {
+        var deletedProduct = await DeleteProductById(id);
+        return deletedProduct;
+    }
+}
