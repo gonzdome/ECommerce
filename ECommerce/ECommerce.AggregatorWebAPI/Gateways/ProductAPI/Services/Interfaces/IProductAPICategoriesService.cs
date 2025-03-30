5cb96b1b9d3a794e77aed648f5a7ffee4be54504
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-30 16:54:35 -0300

    feat: add to AggregatorWebAPI the Service/Interface to deal with Categories Gateway

---
+++ ECommerce/ECommerce.AggregatorWebAPI/Gateways/ProductAPI/Services/Interfaces/IProductAPICategoriesService.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.AggregatorWebAPI.Gateways.ProductAPI.Services.Interfaces;
+
+public interface IProductAPICategoriesService
+{
+    public Task<GetCategoriesViewModelResponse> ProductAPIGetCategories();
+    public Task<DetailCategoryViewModelResponse> ProductAPIDetailCategoryById(string id);
+    public Task<CreateCategoryViewModelResponse> ProductAPICreateCategory(CreateCategoryViewModel product);
+    public Task<UpdateCategoryViewModelResponse> ProductAPIUpdateCategoryById(UpdateCategoryViewModel product);
+    public Task<DetailCategoryViewModelResponse> ProductAPIDeleteCategoryById(string id);
+}
---
+++ ECommerce/ECommerce.AggregatorWebAPI/Gateways/ProductAPI/Services/ProductAPICategoriesService.cs
@@ -0,0 +1,38 @@
+﻿namespace ECommerce.AggregatorWebAPI.Gateways.ProductAPI.Services;
+
+public class ProductAPICategoriesService : ProductAPICategoriesGateway, IProductAPICategoriesService
+{
+    public ProductAPICategoriesService(APIClient apiClient) : base(apiClient)
+    {
+    }
+
+    public async Task<DetailCategoryViewModelResponse> ProductAPIDetailCategoryById(string id)
+    {
+        var detailedCategory = await DetailCategoryById(id);
+        return detailedCategory;
+    }
+
+    public async Task<GetCategoriesViewModelResponse> ProductAPIGetCategories()
+    {
+        var categoriesListed = await GetCategories();
+        return categoriesListed;
+    }
+
+    public async Task<CreateCategoryViewModelResponse> ProductAPICreateCategory(CreateCategoryViewModel product)
+    {
+        var createdCategory = await CreateCategory(product);
+        return createdCategory;
+    }
+
+    public async Task<UpdateCategoryViewModelResponse> ProductAPIUpdateCategoryById(UpdateCategoryViewModel product)
+    {
+        var updatedCategory = await UpdateCategoryById(product);
+        return updatedCategory;
+    }
+
+    public async Task<DetailCategoryViewModelResponse> ProductAPIDeleteCategoryById(string id)
+    {
+        var deletedCategory = await DeleteCategoryById(id);
+        return deletedCategory;
+    }
+}
