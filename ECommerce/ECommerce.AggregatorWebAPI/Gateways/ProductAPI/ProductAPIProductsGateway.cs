8d861da052acddc62e47de4b834992d4be2af709
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-30 17:29:44 -0300

    refactor: creating apiEndpoint variable to abstract standard uri path and reuse it

--- ECommerce/ECommerce.AggregatorWebAPI/Gateways/ProductAPI/ProductAPIProductsGateway.cs
+++ ECommerce/ECommerce.AggregatorWebAPI/Gateways/ProductAPI/ProductAPIProductsGateway.cs
@@ -3,6 +3,7 @@
 public class ProductAPIProductsGateway
 {
     private readonly APIClient _apiClient;
+    private const string apiEndpoint = "/Product";
 
     public ProductAPIProductsGateway(APIClient apiClient)
     {
@@ -11,7 +12,7 @@ public class ProductAPIProductsGateway
 
     protected internal async Task<DetailProductViewModelResponse> DetailProductById(string id)
     {
-        var apiResponse = await _apiClient.Handle("GET", "ProductAPI", $"/Product/GetProductDetailsById?id={id}");
+        var apiResponse = await _apiClient.Handle("GET", "ProductAPI", $"{apiEndpoint}/GetProductDetailsById?id={id}");
 
         var response = new DetailProductViewModelResponse() { Success = apiResponse.Success, Code = apiResponse.Code, Message = apiResponse.Message };
         if (apiResponse.Success == false) return response;
@@ -22,7 +23,7 @@ public class ProductAPIProductsGateway
 
     protected internal async Task<GetProductsViewModelResponse> GetProducts()
     {
-        var apiResponse = await _apiClient.Handle("GET", "ProductAPI", $"/Product/GetProducts");
+        var apiResponse = await _apiClient.Handle("GET", "ProductAPI", $"{apiEndpoint}/GetProducts");
 
         var response = new GetProductsViewModelResponse() { Success = apiResponse.Success, Code = apiResponse.Code, Message = apiResponse.Message };
         if (response.Success == false) return response;
@@ -33,7 +34,7 @@ public class ProductAPIProductsGateway
 
     protected internal async Task<CreateProductViewModelResponse> CreateProduct(CreateProductViewModel productToCreate)
     {
-        var apiResponse = await _apiClient.Handle("POST", "ProductAPI", $"/Product/CreateProduct", productToCreate);
+        var apiResponse = await _apiClient.Handle("POST", "ProductAPI", $"{apiEndpoint}/CreateProduct", productToCreate);
 
         var response = new CreateProductViewModelResponse() { Success = apiResponse.Success, Code = apiResponse.Code, Message = apiResponse.Message };
         if (response.Success == false) return response;
@@ -44,7 +45,7 @@ public class ProductAPIProductsGateway
 
     protected internal async Task<UpdateProductViewModelResponse> UpdateProductById(UpdateProductViewModel productToUpdate)
     {
-        var apiResponse = await _apiClient.Handle("PUT", "ProductAPI", $"/Product/UpdateProductById", productToUpdate);
+        var apiResponse = await _apiClient.Handle("PUT", "ProductAPI", $"{apiEndpoint}/UpdateProductById", productToUpdate);
 
         var response = new UpdateProductViewModelResponse() { Success = apiResponse.Success, Code = apiResponse.Code, Message = apiResponse.Message };
         if (response.Success == false) return response;
@@ -55,7 +56,7 @@ public class ProductAPIProductsGateway
 
     protected internal async Task<DetailProductViewModelResponse> DeleteProductById(string id)
     {
-        var apiResponse = await _apiClient.Handle("DELETE", "ProductAPI", $"/Product/DeleteProductById?id={id}");
+        var apiResponse = await _apiClient.Handle("DELETE", "ProductAPI", $"{apiEndpoint}/DeleteProductById?id={id}");
 
         var response = new DetailProductViewModelResponse() { Success = apiResponse.Success, Code = apiResponse.Code, Message = apiResponse.Message };
         if (apiResponse.Success == false) return response;
