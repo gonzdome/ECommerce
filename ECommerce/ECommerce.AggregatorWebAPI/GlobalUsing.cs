268f1741af08083d3fc67b7d93cd9f58c5670ba8
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-30 16:51:18 -0300

    feat: new usings to GlobalUsing

--- ECommerce/ECommerce.AggregatorWebAPI/GlobalUsing.cs
+++ ECommerce/ECommerce.AggregatorWebAPI/GlobalUsing.cs
@@ -1,11 +1,18 @@
 ﻿global using System.Text;
+global using Newtonsoft.Json;
 global using Microsoft.AspNetCore.Mvc;
 global using System.ComponentModel.DataAnnotations;
 
 global using ECommerce.AggregatorWebAPI.Models.ViewModels.Product;
 global using ECommerce.AggregatorWebAPI.Models.ViewModels.Category;
 
+global using ECommerce.AggregatorWebAPI.Gateways.ProductAPI.Models.ViewModels.Products;
+global using ECommerce.AggregatorWebAPI.Gateways.ProductAPI.Models.ViewModels.Categories;
+
 global using ECommerce.AggregatorWebAPI.Services;
 global using ECommerce.AggregatorWebAPI.Services.Interfaces;
+
+global using ECommerce.AggregatorWebAPI.Gateways.ProductAPI.Services;
+global using ECommerce.AggregatorWebAPI.Gateways.ProductAPI.Services.Interfaces;
 
 global using ECommerce.APIHandler;
