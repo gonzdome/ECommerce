99427703a09a70dc1df43480c003d5f922081f48
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-30 16:55:16 -0300

    feat: add to AggregatorWebAPI the UnitOfWork/Interface to deal with Services and create APIClient Instance

---
+++ ECommerce/ECommerce.AggregatorWebAPI/Gateways/ProductAPI/Services/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace ECommerce.AggregatorWebAPI.Gateways.ProductAPI.Services.Interfaces;
+
+public interface IUnitOfWork
+{
+    public IProductAPIProductsService ProductAPIProductsService { get; }
+    public IProductAPICategoriesService ProductAPICategoriesService { get; }
+}
---
+++ ECommerce/ECommerce.AggregatorWebAPI/Gateways/ProductAPI/Services/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using ECommerce.AggregatorWebAPI.Gateways.ProductAPI.Services.Interfaces;
+using ECommerce.APIHandler;
+
+namespace ECommerce.AggregatorWebAPI.Gateways.ProductAPI.Services
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly APIClient _apiClient;
+        private IProductAPIProductsService? _productAPIProductsService;
+        private IProductAPICategoriesService? _productAPICategoriesService;
+
+        public UnitOfWork(IServiceScopeFactory scoppedFactory, APIClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public IProductAPIProductsService ProductAPIProductsService
+        {
+            get { return _productAPIProductsService = _productAPIProductsService ?? new ProductAPIProductsService(_apiClient); }
+        }
+
+        public IProductAPICategoriesService ProductAPICategoriesService
+        {
+            get { return _productAPICategoriesService = _productAPICategoriesService ?? new ProductAPICategoriesService(_apiClient); }
+        }
+    }
+}
