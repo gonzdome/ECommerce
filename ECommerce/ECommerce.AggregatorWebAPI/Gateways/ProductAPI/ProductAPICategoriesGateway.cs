cd064a9bf52c7c1a8ef1f135d3959f54526cbad4
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-30 16:52:30 -0300

    feat: add to AggregatorWebAPI the gateway to consume ProductAPI and return Categories methods

---
+++ ECommerce/ECommerce.AggregatorWebAPI/Gateways/ProductAPI/ProductAPICategoriesGateway.cs
@@ -0,0 +1,66 @@
+﻿namespace ECommerce.AggregatorWebAPI.Gateways.ProductAPI;
+
+public class ProductAPICategoriesGateway
+{
+    private readonly APIClient _apiClient;
+
+    public ProductAPICategoriesGateway(APIClient apiClient)
+    {
+        _apiClient = apiClient;
+    }
+
+    protected internal async Task<DetailCategoryViewModelResponse> DetailCategoryById(string id)
+    {
+        var apiResponse = await _apiClient.Handle("GET", "ProductAPI", $"/Category/GetCategoryDetailsById?id={id}");
+
+        var response = new DetailCategoryViewModelResponse() { Success = apiResponse.Success, Code = apiResponse.Code, Message = apiResponse.Message };
+        if (apiResponse.Success == false) return response;
+
+        response.Data = JsonConvert.DeserializeObject<DetailCategoryViewModel>(apiResponse.Data);
+        return response;
+    }
+
+    protected internal async Task<GetCategoriesViewModelResponse> GetCategories()
+    {
+        var apiResponse = await _apiClient.Handle("GET", "ProductAPI", $"/Category/GetCategories");
+
+        var response = new GetCategoriesViewModelResponse() { Success = apiResponse.Success, Code = apiResponse.Code, Message = apiResponse.Message };
+        if (response.Success == false) return response;
+
+        response.Data = JsonConvert.DeserializeObject<IEnumerable<GetCategoriesViewModel>>(apiResponse.Data);
+        return response;
+    }
+
+    protected internal async Task<CreateCategoryViewModelResponse> CreateCategory(CreateCategoryViewModel categoryToCreate)
+    {
+        var apiResponse = await _apiClient.Handle("POST", "ProductAPI", $"/Category/CreateCategory", categoryToCreate);
+
+        var response = new CreateCategoryViewModelResponse() { Success = apiResponse.Success, Code = apiResponse.Code, Message = apiResponse.Message };
+        if (response.Success == false) return response;
+
+        response.Data = JsonConvert.DeserializeObject<CreateCategoryViewModel>(apiResponse.Data);
+        return response;
+    }
+
+    protected internal async Task<UpdateCategoryViewModelResponse> UpdateCategoryById(UpdateCategoryViewModel categoryToUpdate)
+    {
+        var apiResponse = await _apiClient.Handle("PUT", "ProductAPI", $"/Category/UpdateCategoryById", categoryToUpdate);
+
+        var response = new UpdateCategoryViewModelResponse() { Success = apiResponse.Success, Code = apiResponse.Code, Message = apiResponse.Message };
+        if (response.Success == false) return response;
+
+        response.Data = JsonConvert.DeserializeObject<UpdateCategoryViewModel>(apiResponse.Data);
+        return response;
+    }
+
+    protected internal async Task<DetailCategoryViewModelResponse> DeleteCategoryById(string id)
+    {
+        var apiResponse = await _apiClient.Handle("DELETE", "ProductAPI", $"/Category/DeleteCategoryById?id={id}");
+
+        var response = new DetailCategoryViewModelResponse() { Success = apiResponse.Success, Code = apiResponse.Code, Message = apiResponse.Message };
+        if (apiResponse.Success == false) return response;
+
+        response.Data = JsonConvert.DeserializeObject<DetailCategoryViewModel>(apiResponse.Data);
+        return response;
+    }
+}
