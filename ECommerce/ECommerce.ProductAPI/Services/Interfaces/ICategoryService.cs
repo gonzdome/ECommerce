8be37907fce5b89efa9461b11dbfd09fb9016f21
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-30 17:05:23 -0300

    fix: adjustment on ProductAPI ICategoryService/ProductService UpdateProductById params

--- ECommerce/ECommerce.ProductAPI/Services/Interfaces/ICategoryService.cs
+++ ECommerce/ECommerce.ProductAPI/Services/Interfaces/ICategoryService.cs
@@ -5,6 +5,6 @@ public interface ICategoryService
     public Task<IEnumerable<CategoryDTO>> GetCategories();
     public Task<CategoryDTO> DetailCategoryById(string id);
     public Task<CategoryDTO> CreateCategory(CategoryDTO purchase);
-    public Task<CategoryDTO> UpdateCategoryById(string id, CategoryDTO purchase);
+    public Task<CategoryDTO> UpdateCategoryById(CategoryDTO purchase);
     public Task<CategoryDTO> DeleteCategoryById(string id);
 }
--- ECommerce/ECommerce.ProductAPI/Services/Interfaces/IProductService.cs
+++ ECommerce/ECommerce.ProductAPI/Services/Interfaces/IProductService.cs
@@ -5,6 +5,6 @@ public interface IProductService
     public Task<IEnumerable<ProductDTO>> GetProducts();
     public Task<ProductDTO> DetailProductById(string id);
     public Task<ProductDTO> CreateProduct(ProductDTO product);
-    public Task<ProductDTO> UpdateProductById(string id, ProductDTO product);
+    public Task<ProductDTO> UpdateProductById(ProductDTO product);
     public Task<ProductDTO> DeleteProductById(string id);
 }
