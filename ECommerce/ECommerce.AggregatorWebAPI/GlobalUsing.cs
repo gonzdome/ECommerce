06348b0b04d540555a4c9c8d53a51ac67622288d
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-31 23:53:03 -0300

    feat: add new usings to GlobalUsing

--- ECommerce/ECommerce.AggregatorWebAPI/GlobalUsing.cs
+++ ECommerce/ECommerce.AggregatorWebAPI/GlobalUsing.cs
@@ -1,18 +1,26 @@
-﻿global using System.Text;
-global using Newtonsoft.Json;
+﻿global using Newtonsoft.Json;
 global using Microsoft.AspNetCore.Mvc;
 global using System.ComponentModel.DataAnnotations;
-
-global using ECommerce.AggregatorWebAPI.Models.ViewModels.Product;
-global using ECommerce.AggregatorWebAPI.Models.ViewModels.Category;
-
-global using ECommerce.AggregatorWebAPI.Gateways.ProductAPI.Models.ViewModels.Products;
-global using ECommerce.AggregatorWebAPI.Gateways.ProductAPI.Models.ViewModels.Categories;
 
 global using ECommerce.AggregatorWebAPI.Services;
 global using ECommerce.AggregatorWebAPI.Services.Interfaces;
 
+global using ECommerce.AggregatorWebAPI.Models.ViewModels.Product;
+global using ECommerce.AggregatorWebAPI.Models.ViewModels.Category;
+global using ECommerce.AggregatorWebAPI.Models.ViewModels.Purchase;
+
+global using ECommerce.AggregatorWebAPI.Gateways.ProductAPI.Models.ViewModels.Products;
+global using ECommerce.AggregatorWebAPI.Gateways.ProductAPI.Models.ViewModels.Categories;
+
+global using ECommerce.AggregatorWebAPI.Gateways.IntegrationAPI.Models.ViewModels;
+
+global using ECommerce.AggregatorWebAPI.Gateways.Services.Interfaces;
+
 global using ECommerce.AggregatorWebAPI.Gateways.ProductAPI.Services;
 global using ECommerce.AggregatorWebAPI.Gateways.ProductAPI.Services.Interfaces;
+
+global using ECommerce.AggregatorWebAPI.Gateways.IntegrationAPI.Services;
+global using ECommerce.AggregatorWebAPI.Gateways.IntegrationAPI.Services.Interfaces;
+
 
 global using ECommerce.APIHandler;
