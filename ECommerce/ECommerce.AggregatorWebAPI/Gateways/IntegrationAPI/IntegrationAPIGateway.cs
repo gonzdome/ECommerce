0a92f3b790ca119fea7ac5f1cc934822f8e7b09e
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-31 23:56:31 -0300

    feat: add on AggregatorWebAPI the IntegrationAPI gateway

---
+++ ECommerce/ECommerce.AggregatorWebAPI/Gateways/IntegrationAPI/IntegrationAPIGateway.cs
@@ -0,0 +1,23 @@
+﻿namespace ECommerce.AggregatorWebAPI.Gateways.IntegrationAPI;
+
+public class IntegrationAPIGateway
+{
+    private readonly APIClient _apiClient;
+    private const string apiEndpoint = "https://localhost:0000/Purchase";
+
+    public IntegrationAPIGateway(APIClient apiClient)
+    {
+        _apiClient = apiClient;
+    }
+
+    protected internal async Task<SendPurchaseViewModelResponse> IntegrationAPIPurchase(SendPurchaseViewModel IntegrationAPIPostRequest)
+    {
+        var apiResponse = await _apiClient.Handle("POST", "IntegrationAPI", $"{apiEndpoint}/SendPurchase", IntegrationAPIPostRequest);
+
+        var response = new SendPurchaseViewModelResponse() { Success = apiResponse.Success, Code = apiResponse.Code, Message = apiResponse.Message };
+        if (apiResponse.Success == false) return response;
+
+        response.Data = JsonConvert.DeserializeObject<SendPurchaseViewModelResponse>(apiResponse.Data);
+        return response;
+    }
+}
