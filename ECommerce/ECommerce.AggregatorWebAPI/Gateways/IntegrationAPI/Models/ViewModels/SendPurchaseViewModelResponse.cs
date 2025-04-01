0dfbf469312580f4ad785ab0b48cb3e86ee9e63e
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-31 23:55:28 -0300

    feat: add on AggregatorWebAPI the view models for Purchase methods

---
+++ ECommerce/ECommerce.AggregatorWebAPI/Gateways/IntegrationAPI/Models/ViewModels/SendPurchaseViewModelResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.AggregatorWebAPI.Gateways.IntegrationAPI.Models.ViewModels;
+
+public class SendPurchaseViewModelResponse
+{
+    public bool Success { get; set; }
+    public string Message { get; set; }
+    public int Code { get; set; }
+
+    public object Data { get; set; }
+}
---
+++ ECommerce/ECommerce.AggregatorWebAPI/Models/ViewModels/Purchase/SendPurchaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ECommerce.AggregatorWebAPI.Models.ViewModels.Purchase;
+
+public class SendPurchaseViewModel
+{
+    public string identificador { get; set; }
+    public double subTotal { get; set; }
+    public double descontos { get; set; }
+    public double valorTotal { get; set; }
+    public List<Items> itens { get; set; }
+
+    public class Items
+    {
+        public double quantidade { get; set; }
+        public double precoUnitario { get; set; }
+    }
+}
