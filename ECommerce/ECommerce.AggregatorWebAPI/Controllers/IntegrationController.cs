f601d4c29a69f223398e1176b05398a0d84fbaa8
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-31 23:50:46 -0300

    feat: add to AggregatorWebAPI IntegrationController

---
+++ ECommerce/ECommerce.AggregatorWebAPI/Controllers/IntegrationController.cs
@@ -0,0 +1,30 @@
+﻿using ECommerce.AggregatorWebAPI.Gateways.IntegrationAPI.Models.ViewModels;
+
+namespace ECommerce.AggregatorWebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class IntegrationController : ControllerBase
+{
+    private readonly IIntegrationService _categoryService;
+
+    public IntegrationController(IIntegrationService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+
+    [HttpPost]
+    [Route("SendPurchase")]
+    public async Task<ActionResult<SendPurchaseViewModelResponse>> SendPurchase([FromBody] SendPurchaseViewModel categoryPayload)
+    {
+        try
+        {
+            var category = await _categoryService.SendPurchase(categoryPayload);
+            return Ok(category);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e);
+        }
+    }
+}
