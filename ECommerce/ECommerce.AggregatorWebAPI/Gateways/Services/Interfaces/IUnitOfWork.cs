927cd212520e7e1530c82b146b516661736e0b43
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-31 23:54:38 -0300

    refactor: change on AggregatorWebAPI the UnitOfWork/Integrationplace on Gateways folder

--- ECommerce/ECommerce.AggregatorWebAPI/Gateways/Services/Interfaces/IUnitOfWork.cs
+++ ECommerce/ECommerce.AggregatorWebAPI/Gateways/Services/Interfaces/IUnitOfWork.cs
@@ -1,7 +1,8 @@
-﻿namespace ECommerce.AggregatorWebAPI.Gateways.ProductAPI.Services.Interfaces;
+﻿namespace ECommerce.AggregatorWebAPI.Gateways.Services.Interfaces;
 
 public interface IUnitOfWork
 {
     public IProductAPIProductsService ProductAPIProductsService { get; }
     public IProductAPICategoriesService ProductAPICategoriesService { get; }
+    public IIntegrationAPIService IntegrationAPIService { get; }
 }
---
+++ ECommerce/ECommerce.AggregatorWebAPI/Gateways/Services/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+
+namespace ECommerce.AggregatorWebAPI.Gateways.Services;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly APIClient _apiClient;
+    private IProductAPIProductsService? _productAPIProductsService;
+    private IProductAPICategoriesService? _productAPICategoriesService;
+    private IIntegrationAPIService? _IntegrationAPIService;
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly JsonSerializerOptions _options;
+
+    public UnitOfWork(IHttpClientFactory httpClientFactory, APIClient apiClient)
+    {
+        _httpClientFactory = httpClientFactory;
+        _apiClient = new APIClient(_httpClientFactory);
+    }
+
+    public IProductAPIProductsService ProductAPIProductsService
+    {
+        get { return _productAPIProductsService = _productAPIProductsService ?? new ProductAPIProductsService(_apiClient); }
+    }
+
+    public IProductAPICategoriesService ProductAPICategoriesService
+    {
+        get { return _productAPICategoriesService = _productAPICategoriesService ?? new ProductAPICategoriesService(_apiClient); }
+    }
+
+    public IIntegrationAPIService IntegrationAPIService
+    {
+        get { return _IntegrationAPIService = _IntegrationAPIService ?? new IntegrationAPIService(_apiClient); }
+    }
+
+}
