6a65885e1aef24a0701fdef6bc2e858c05d64a6c
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-03-31 23:49:20 -0300

    feat: add to IntegrationAPI Purchase Gateway Service/Interface

---
+++ ECommerce/ECommerce.IntegrationAPI/Gateways/PurchaseAPI/Services/Interfaces/IPurchaseAPIService.cs
@@ -0,0 +1,6 @@
+﻿namespace ECommerce.IntegrationAPI.Gateways.PurchaseAPI.Services.Interfaces;
+
+public interface IPurchaseAPIService
+{
+    public Task<PurchaseAPIPostResponse> PurchaseAPISend(PurchaseAPIPostRequest purchaseAPIPostRequest);
+}
---
+++ ECommerce/ECommerce.IntegrationAPI/Gateways/PurchaseAPI/Services/PurchaseAPIService.cs
@@ -0,0 +1,16 @@
+﻿using ECommerce.IntegrationAPI.Gateways.PurchaseAPI.Models.ViewModels.Purchase;
+
+namespace ECommerce.IntegrationAPI.Gateways.PurchaseAPI.Services;
+
+public class PurchaseAPIService : PurchaseAPIGateway, IPurchaseAPIService
+{
+    public PurchaseAPIService(APIClient apiClient) : base(apiClient)
+    {
+    }
+
+    public async Task<PurchaseAPIPostResponse> PurchaseAPISend(PurchaseAPIPostRequest purchaseAPIPostRequest)
+    {
+        var purchaseResponse = await Purchase(purchaseAPIPostRequest);
+        return purchaseResponse;
+    }
+}
