0ed64bdfca6cf0bf05c853a14e9aad5e73ccc52f
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-04-01 22:24:04 -0300

    feat: on IntegrationAPI add CommonRepository and Interface

---
+++ ECommerce/ECommerce.IntegrationAPI/Repositories/Interfaces/ICommonRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace ECommerce.IntegrationAPI.Repositories.Interfaces;
+
+public interface ICommonRepository<T>
+{
+    Task<IEnumerable<T>> GetAll();
+    Task<T> Details(Expression<Func<T, bool>> predicate);
+    Task<T> Create(T entity);
+    Task<T> Update(T entity);
+    Task<T> Delete(T entity);
+}
