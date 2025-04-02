016c12985c4c55d7aaa9d257c723f768e9435b99
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-04-01 22:13:03 -0300

    feat: on AggregatorWebAPI new ViewModels for Integration/Purchase flow

---
+++ ECommerce/ECommerce.AggregatorWebAPI/Models/ViewModels/Integration/CreateIntegrationViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ECommerce.AggregatorWebAPI.Models.ViewModels.Integration;
+
+public class CreateIntegrationViewModel
+{
+    [MinLength(3)]
+    [MaxLength(100)]
+    public string Name { get; set; }
+
+    [MinLength(3)]
+    [MaxLength(300)]
+    public string Flow { get; set; }
+
+    [MinLength(3)]
+    [MaxLength(500)]
+    public string Uri { get; set; }
+}
---
+++ ECommerce/ECommerce.AggregatorWebAPI/Models/ViewModels/Integration/DetailIntegrationViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace ECommerce.AggregatorWebAPI.Models.ViewModels.Integration;
+
+public class DetailIntegrationViewModel
+{
+    public Guid Id { get; set; }
+
+    [MinLength(3)]
+    [MaxLength(100)]
+    public string Name { get; set; }
+
+    [MinLength(3)]
+    [MaxLength(300)]
+    public string Flow { get; set; }
+
+    [MinLength(3)]
+    [MaxLength(500)]
+    public string Uri { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+}
