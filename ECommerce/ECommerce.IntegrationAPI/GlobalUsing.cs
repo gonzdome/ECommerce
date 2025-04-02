12b08ad31d7808a2274aa87d8ff95707634c4d63
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-04-01 22:43:25 -0300

    feat: on IntegrationAPI add new usings to GlobalUsing

--- ECommerce/ECommerce.IntegrationAPI/GlobalUsing.cs
+++ ECommerce/ECommerce.IntegrationAPI/GlobalUsing.cs
@@ -1,13 +1,28 @@
 ﻿global using Newtonsoft.Json;
 global using Microsoft.AspNetCore.Mvc;
+global using System.ComponentModel.DataAnnotations;
+
+global using ECommerce.IntegrationAPI.Context;
+global using Microsoft.EntityFrameworkCore;
+
+global using ECommerce.IntegrationAPI.DTO;
+global using ECommerce.IntegrationAPI.DTO.DTOMapping;
+
+global using ECommerce.IntegrationAPI.Models.Entities;
 
 global using ECommerce.IntegrationAPI.Services;
 global using ECommerce.IntegrationAPI.Services.Interfaces;
+
+global using ECommerce.IntegrationAPI.Repositories;
+global using ECommerce.IntegrationAPI.Repositories.Interfaces;
 
 global using ECommerce.IntegrationAPI.Gateways.Models.ViewModels;
 global using ECommerce.IntegrationAPI.Gateways.PurchaseAPI.Models.ViewModels.Purchase;
 
 global using ECommerce.IntegrationAPI.Gateways.PurchaseAPI.Services;
 global using ECommerce.IntegrationAPI.Gateways.PurchaseAPI.Services.Interfaces;
+
+global using ECommerce.IntegrationAPI.Gateways.Services;
+global using ECommerce.IntegrationAPI.Gateways.Services.Interfaces;
 
 global using ECommerce.APIHandler;
