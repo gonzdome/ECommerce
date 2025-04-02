ffbbf4d904d22ddbb9b5f1c62c1ef6d03fdb8dd8
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-04-01 22:21:48 -0300

    feat: on IntegrationAPI add AppDbContext

---
+++ ECommerce/ECommerce.IntegrationAPI/Context/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.IntegrationAPI.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Integration> Integrations { get; set; }
+}
