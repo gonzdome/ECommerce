3ddec3e0ed5251a969ce012036a9d7b4969fea70
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-04-01 22:24:19 -0300

    feat: on IntegrationAPI add IntegrationRepository and Interface

---
+++ ECommerce/ECommerce.IntegrationAPI/Repositories/IntegrationRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerce.IntegrationAPI.Repositories;
+
+public class IntegrationRepository : CommonRepository<Integration>, IIntegrationRepository
+{
+    public IntegrationRepository(AppDbContext context) : base(context)
+    {
+    }
+}
---
+++ ECommerce/ECommerce.IntegrationAPI/Repositories/Interfaces/IIntegrationRepository.cs
@@ -0,0 +1,5 @@
+﻿namespace ECommerce.IntegrationAPI.Repositories.Interfaces;
+
+public interface IIntegrationRepository : ICommonRepository<Integration>
+{
+}
