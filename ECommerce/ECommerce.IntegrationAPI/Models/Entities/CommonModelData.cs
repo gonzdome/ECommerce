cab27f50c2e8a18dfd42b7153c900950cb489dce
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-04-01 22:23:34 -0300

    feat: on IntegrationAPI add CommonModelData

---
+++ ECommerce/ECommerce.IntegrationAPI/Models/Entities/CommonModelData.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.IntegrationAPI.Models.Entities;
+
+public class CommonModelData
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public bool Active { get; set; } = true;
+    public bool Excluded { get; set; } = false;
+}
