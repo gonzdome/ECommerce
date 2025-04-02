327a0de9e4507e414d317d3f62db42435f03ef4d
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-04-01 22:22:03 -0300

    feat: on IntegrationAPI add IntegrationController

---
+++ ECommerce/ECommerce.IntegrationAPI/Controllers/IntegrationController.cs
@@ -0,0 +1,84 @@
+﻿namespace ECommerce.IntegrationAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class IntegrationController : ControllerBase
+{
+    private readonly IIntegrationService _integrationService;
+
+    public IntegrationController(IIntegrationService integrationService)
+    {
+        _integrationService = integrationService;
+    }
+
+    [HttpGet("GetIntegrations")]
+    public async Task<ActionResult<IEnumerable<IntegrationDTO>>> GetIntegrations()
+    {
+        try
+        {
+            var integration = await _integrationService.GetIntegrations();
+            return Ok(integration);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e);
+        }
+    }
+
+    [HttpGet("GetIntegrationDetailsByFlow")]
+    public async Task<ActionResult<IntegrationDTO>> GetIntegrationDetailsByFlow([FromQuery] string flowName)
+    {
+        try
+        {
+            var integration = await _integrationService.DetailIntegrationByFlow(flowName);
+            return Ok(integration);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e);
+        }
+    }
+
+    [HttpPost]
+    [Route("CreateIntegration")]
+    public async Task<ActionResult<IntegrationDTO>> CreateIntegration(IntegrationDTO integrationPayload)
+    {
+        try
+        {
+            var integration = await _integrationService.CreateIntegration(integrationPayload);
+            return Ok(integration);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e);
+        }
+    }
+
+    [HttpPut("UpdateIntegrationById")]
+    public async Task<ActionResult<IntegrationDTO>> UpdateIntegrationById(IntegrationDTO integrationToUpdate)
+    {
+        try
+        {
+            var integration = await _integrationService.UpdateIntegrationById(integrationToUpdate);
+            return integration;
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpDelete("DeleteIntegrationById")]
+    public async Task<ActionResult<IntegrationDTO>> DeleteIntegrationById([FromQuery] string id)
+    {
+        try
+        {
+            var integration = await _integrationService.DeleteIntegrationById(id);
+            return Ok(integration);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e);
+        }
+    }
+}
