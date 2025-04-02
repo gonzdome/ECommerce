e9e1544fce9e0c6d9e9b1567ab98a173706fc68f
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-04-01 22:13:55 -0300

    feat: on AggregatorWebAPI add PurchaseController

---
+++ ECommerce/ECommerce.AggregatorWebAPI/Controllers/PurchaseController.cs
@@ -0,0 +1,37 @@
+﻿using ECommerce.AggregatorWebAPI.Gateways.IntegrationAPI.Models.ViewModels.Purchase;
+
+namespace ECommerce.AggregatorWebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PurchaseController : ControllerBase
+{
+    private readonly IPurchaseService _purchaseService;
+    private readonly IProductService _productService;
+    private readonly ICategoryService _categoryService;
+
+    public PurchaseController(IPurchaseService purchaseService, IProductService productService, ICategoryService categoryService)
+    {
+        _purchaseService = purchaseService;
+        _categoryService = categoryService;
+        _productService = productService;
+    }
+
+    [HttpPost]
+    [Route("SendPurchase")]
+    public async Task<ActionResult<SendPurchaseViewModelResponse>> SendPurchase([FromBody] SendPurchaseViewModel purchasePayload)
+    {
+        try
+        {
+            foreach (var item in purchasePayload.itens)
+                await _productService.DetailProductById(item.produtoId);
+
+            var purchase = await _purchaseService.SendPurchase(purchasePayload);
+            return Ok(purchase);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e);
+        }
+    }
+}
