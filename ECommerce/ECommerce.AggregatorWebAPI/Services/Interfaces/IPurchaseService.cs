312d75ef7b3bdca02cfe5f83c13f3b5f431a2153
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-04-01 22:15:20 -0300

    feat: on AggregatorWebAPI add PurchaseService/Interface

---
+++ ECommerce/ECommerce.AggregatorWebAPI/Services/Interfaces/IPurchaseService.cs
@@ -0,0 +1,8 @@
+﻿using ECommerce.AggregatorWebAPI.Gateways.IntegrationAPI.Models.ViewModels.Purchase;
+
+namespace ECommerce.AggregatorWebAPI.Services.Interfaces;
+
+public interface IPurchaseService
+{
+    Task<SendPurchaseViewModelResponse> SendPurchase(SendPurchaseViewModel request);
+}
---
+++ ECommerce/ECommerce.AggregatorWebAPI/Services/PurchaseService.cs
@@ -0,0 +1,19 @@
+﻿using ECommerce.AggregatorWebAPI.Gateways.IntegrationAPI.Models.ViewModels.Purchase;
+
+namespace ECommerce.AggregatorWebAPI.Services;
+
+public class PurchaseService : IPurchaseService
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public PurchaseService(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<SendPurchaseViewModelResponse> SendPurchase(SendPurchaseViewModel request)
+    {
+        var result = await _unitOfWork.IntegrationAPIPurchaseService.IntegrationAPISendPurchase(request);
+        return new SendPurchaseViewModelResponse();
+    }
+}
