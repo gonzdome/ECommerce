4810f9ca38aa430e6f349ca3bdc2b487e031ce18
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-04-01 22:23:45 -0300

    feat: on IntegrationAPI add Integration Model

---
+++ ECommerce/ECommerce.IntegrationAPI/Models/Entities/Integration.cs
@@ -0,0 +1,16 @@
+﻿namespace ECommerce.IntegrationAPI.Models.Entities;
+
+public class Integration : CommonModelData
+{
+    [MinLength(3)]
+    [MaxLength(100)]
+    public string Name { get; set; }
+
+    [MinLength(3)]
+    [MaxLength(300)]
+    public string Flow { get; set; }
+
+    [MinLength(3)]
+    [MaxLength(500)]
+    public string Uri { get; set; }
+}
