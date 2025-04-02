0d5761f5ebfddce93ee010bf1b0885f99bacc67d
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-04-01 22:22:16 -0300

    feat: on IntegrationAPI add IntegrationDTO

---
+++ ECommerce/ECommerce.IntegrationAPI/DTO/IntegrationDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace ECommerce.IntegrationAPI.DTO;
+
+public class IntegrationDTO
+{
+    public Guid Id { get; set; }
+
+    [MinLength(3)]
+    [MaxLength(100)]
+    public string Name { get; set; }
+
+    [MinLength(3)]
+    [MaxLength(300)]
+    public string Flow { get; set; }
+
+    [MinLength(3)]
+    [MaxLength(500)]
+    public string Uri { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public bool Active { get; set; } = true;
+    public bool Excluded { get; set; } = false;
+}
