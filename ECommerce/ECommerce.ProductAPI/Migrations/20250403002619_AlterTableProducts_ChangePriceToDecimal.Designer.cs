5473aeac3f892050cfa8f30f59f05de00065d79d
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-04-02 21:40:57 -0300

    fix: changed Product Price to Decimal

--- ECommerce/ECommerce.ProductAPI/Migrations/20250403002619_AlterTableProducts_ChangePriceToDecimal.Designer.cs
+++ ECommerce/ECommerce.ProductAPI/Migrations/20250403002619_AlterTableProducts_ChangePriceToDecimal.Designer.cs
@@ -4,6 +4,7 @@ using ECommerce.ProductAPI.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ECommerce.ProductAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250403002619_AlterTableProducts_ChangePriceToDecimal")]
+    partial class AlterTableProducts_ChangePriceToDecimal
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
